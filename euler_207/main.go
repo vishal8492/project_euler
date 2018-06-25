8169651f35f04c2d0ec90bf071295a3569bd494e
Vishal <vishal.ithape@mobikontech.com>
2018-06-25 14:03:32 +0530

    initial go at this 207, pun` intended :)

--- euler_207/main.go
+++ euler_207/main.go
@@ -1,35 +1,76 @@
-package euler_207
+package main
 
 import (
+	"fmt"
 	"math"
-	"log"
 )
 
 func main() {
-	m := 185
+	var input int
+	fmt.Scanf("%d", &input)
+	var lms = make([]int, input)
+	var ms = make([]float64, input)
+
+	for i := 0; i < input; i++ {
+		//fmt.Printf("Inside scan")
+		var a int
+		var b int
+		fmt.Scanf("%d", &a)
+		fmt.Scanf("%d", &b)
+		ms[i] = float64(a) / float64(b)
+	}
+	m := math.MaxInt32
 	//k := 2
-	total := 1
-	perfect := 1
 
 	/**
-	4^t = 2^t  + k
-	k starts from 1
-	2^t starts from 3
-	k<=m
-	 */
+4^t = 2^t  + k
+k starts from 1
+2^t starts from 3
+k<=m
+ */
+	mtemp := 3
+	cnt := 0
 
-	for l := 3; l <= m; l++ {
-		ktemp := l*l - l
+	for mtemp != m {
+		total := 1
+		perfect := 1
+		for l := 3; l <= m; l++ {
+			ktemp := l*l - l
 
-		if ktemp <= m {
-			//check if log l is integer
-			base := math.Log2(float64(l))
-			if base == math.Trunc(base) {
-				perfect += 1
+			if ktemp <= mtemp {
+				//check if log l is integer
+				base := math.Log2(float64(l))
+				if base == math.Trunc(base) {
+					perfect += 1
+				}
+				total += 1
+
+				for i, thresh := range ms {
+					if thresh > (float64(perfect) / float64(total)) {
+						if lms[i] == 0 {
+							lms[i] = mtemp
+							cnt += 1
+						}
+					}
+				}
+
+			} else {
+				break
 			}
-			total += 1
-		}
+			if cnt == input {
+				break
+			}
 
+		}
+		//log.Printf("total:%v,perfect:%v", total, perfect)
+
+		mtemp += 1
+		if cnt == input {
+			break
+		}
 	}
-	log.Printf("total:%v,perfect:%v", total, perfect)
+
+	for _, v := range lms {
+		fmt.Println(v)
+	}
 }
